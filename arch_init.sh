d512e80d61e63e842b75a467fc60174c0701f517
psichen <psichen@outlook.com>
2023-08-01 22:49:02 -0400

    sync

--- arch_init.sh
+++ arch_init.sh
@@ -103,4 +103,5 @@ fc-cache -f # refresh fonts
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' # vim-plug
 python -m venv $HOME/Documents/dev # python virtual environment
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # oh-my-zsh
+sudo systemctl enable sshd
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
